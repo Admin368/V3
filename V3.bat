6afd29a4813fc13d64a0bd5e40d0905e10654bcf
Admin368_Legendt3 <58757588+Admin368@users.noreply.github.com>
2021-03-02 21:36:37 +0800

    V3_Update

--- V3.bat
+++ V3.bat
@@ -9,7 +9,7 @@ Echo %ExecPol%
 IF "%ExecPol%"=="Restricted" Echo EXECUTION_POLICY : FAILED - ExecutionPolicy !!RESTRICTED, V3 will Enable && Pause && Echo V3 - Enabling Execution && Powershell Set-ExecutionPolicy -Scope CurrentUser Unrestricted && goto :ExecutionPolicy
 IF "%ExecPol%"=="Unrestricted" Echo [EXECUTION_POLICY : PASSED - ExecutionPolicy UNRESTRICTED
 Rem Powershell Set-ExecutionPolicy -Scope CurrentUser restricted
-PowerShell .\V3_Main.ps1
+PowerShell .\Application\V3_Main.ps1
 Rem PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File "".\V3_Main.ps1""' -Verb RunAs}" -NoNewWindow;
 Rem PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File ""V3_Main.ps1""' -Verb RunAs}";
 Rem C:\Users\Legend68\Documents\WPS Cloud Files\409081444\$XUL_FILES\Keanu_V3
